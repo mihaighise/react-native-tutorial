c05878dfa754ee31193f96856e534ead670b8140
mihaighise <mihaighise1@gmail.com>
2023-11-12 17:56:50 +0200

    Styling with flex

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { useDeviceOrientation } from '@react-native-community/hooks';
 import { StatusBar } from 'expo-status-bar';
-import { Platform, SafeAreaView, StyleSheet, View } from 'react-native';
+import { Platform, StyleSheet, View } from 'react-native';
 
 export default function App() {
 
@@ -8,12 +8,54 @@ export default function App() {
   console.log('orientation is:', orientation)
 
   return (
-    <SafeAreaView style={styles.container}>
+    // <SafeAreaView style={styles.container}>
+    //   {/* <View style={{
+    //     backgroundColor: 'dodgerblue', width: '100%',
+    //     height: orientation === 'landscape' ? '100%' : '30%'
+    //   }}></View> */}
+    //   <View style={{ backgroundColor: 'green', flex: 0.5, height: 200 }}></View>
+    // </SafeAreaView>
+    <View style={{
+      backgroundColor: "#fff",
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      alignContent: 'center',
+      flexWrap: 'wrap'
+    }}>
       <View style={{
-        backgroundColor: 'dodgerblue', width: '100%',
-        height: orientation === 'landscape' ? '100%' : '30%'
-      }}></View>
-    </SafeAreaView>
+        backgroundColor: 'dodgerblue',
+        // flex: 1,
+        width: 100,
+        height: 300,
+        alignSelf: 'flex-start'
+      }} />
+      <View style={{
+        backgroundColor: 'gold',
+        // flex: 1,
+        width: 100,
+        height: 200
+      }} />
+      <View style={{
+        backgroundColor: 'tomato',
+        // flex: 1,
+        width: 100,
+        height: 100
+      }} />
+      <View style={{
+        backgroundColor: 'grey',
+        // flex: 1,
+        width: 100,
+        height: 100
+      }} />
+      <View style={{
+        backgroundColor: 'greenyellow',
+        // flex: 1,
+        width: 100,
+        height: 100
+      }} />
+    </View>
   );
 }
 
