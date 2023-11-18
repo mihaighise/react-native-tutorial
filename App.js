d0f62548a8ae7d92ec41bf0c5dc9e4b3f73306e5
mihaighise <mihaighise1@gmail.com>
2023-11-18 16:13:27 +0200

    API response placed in a list

--- App.js
+++ App.js
@@ -1,70 +1,93 @@
 import { useDeviceOrientation } from '@react-native-community/hooks';
-import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Platform, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [postList, setPostList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
 
   const orientation = useDeviceOrientation()
   console.log('orientation is:', orientation)
 
+  const fetchData = async (limit = 10) => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
+    const data = await response.json();
+    setPostList(data);
+    setIsLoading(false);
+  }
+
+  const handleRefresh = () => {
+    setRefreshing(true);
+    fetchData(20);
+    setRefreshing(false);
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetchData();
+    }, 1500);
+  }, [])
+
+  if (isLoading) {
+    return (
+      <SafeAreaView style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="0000ff"></ActivityIndicator>
+        <Text>Loading...</Text>
+      </SafeAreaView>
+    )
+  }
+
   return (
-    // <SafeAreaView style={styles.container}>
-    //   {/* <View style={{
-    //     backgroundColor: 'dodgerblue', width: '100%',
-    //     height: orientation === 'landscape' ? '100%' : '30%'
-    //   }}></View> */}
-    //   <View style={{ backgroundColor: 'green', flex: 0.5, height: 200 }}></View>
-    // </SafeAreaView>
-    <View style={{
-      backgroundColor: "#fff",
-      flex: 1,
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-      alignContent: 'center',
-      flexWrap: 'wrap'
-    }}>
-      <View style={{
-        backgroundColor: 'dodgerblue',
-        // flex: 1,
-        width: 100,
-        height: 300,
-        alignSelf: 'flex-start'
-      }} />
-      <View style={{
-        backgroundColor: 'gold',
-        // flex: 1,
-        width: 100,
-        height: 200
-      }} />
-      <View style={{
-        backgroundColor: 'tomato',
-        // flex: 1,
-        width: 100,
-        height: 100
-      }} />
-      <View style={{
-        backgroundColor: 'grey',
-        // flex: 1,
-        width: 100,
-        height: 100
-      }} />
-      <View style={{
-        backgroundColor: 'greenyellow',
-        // flex: 1,
-        width: 100,
-        height: 100
-      }} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.listContainer}>
+        <FlatList
+          data={postList}
+          refreshing={refreshing}
+          onRefresh={handleRefresh}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.card}>
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.body}>{item.body}</Text>
+              </View>
+            )
+          }} />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
   },
+  listContainer: {
+    flex: 1,
+    paddingHorizontal: 16
+  },
+  card: {
+    backgroundColor: 'white',
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    margin: 10
+  },
+  title: {
+    fontSize: 30
+  },
+  body: {
+    fontSize: 24,
+    color: '#666666'
+  },
+  loadingContainer: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+    paddingTop: StatusBar.currentHeight,
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
