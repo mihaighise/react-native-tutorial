35d73eb14d0fa6ddcefd9c935df2c64ce07403af
mihaighise <mihaighise1@gmail.com>
2023-11-18 16:32:13 +0200

    Stack navigation with prop or hook

--- App.js
+++ App.js
@@ -1,93 +1,17 @@
-import { useDeviceOrientation } from '@react-native-community/hooks';
-import { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, Platform, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AboutScreen from "./screens/AboutScreen";
+import HomeScreen from "./screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-
-  const [postList, setPostList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [refreshing, setRefreshing] = useState(false);
-
-  const orientation = useDeviceOrientation()
-  console.log('orientation is:', orientation)
-
-  const fetchData = async (limit = 10) => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
-    const data = await response.json();
-    setPostList(data);
-    setIsLoading(false);
-  }
-
-  const handleRefresh = () => {
-    setRefreshing(true);
-    fetchData(20);
-    setRefreshing(false);
-  }
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetchData();
-    }, 1500);
-  }, [])
-
-  if (isLoading) {
-    return (
-      <SafeAreaView style={styles.loadingContainer}>
-        <ActivityIndicator size="large" color="0000ff"></ActivityIndicator>
-        <Text>Loading...</Text>
-      </SafeAreaView>
-    )
-  }
-
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.listContainer}>
-        <FlatList
-          data={postList}
-          refreshing={refreshing}
-          onRefresh={handleRefresh}
-          renderItem={({ item }) => {
-            return (
-              <View style={styles.card}>
-                <Text style={styles.title}>{item.title}</Text>
-                <Text style={styles.body}>{item.body}</Text>
-              </View>
-            )
-          }} />
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='About' component={AboutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f5f5f5',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
-  },
-  listContainer: {
-    flex: 1,
-    paddingHorizontal: 16
-  },
-  card: {
-    backgroundColor: 'white',
-    padding: 16,
-    borderRadius: 8,
-    borderWidth: 1,
-    margin: 10
-  },
-  title: {
-    fontSize: 30
-  },
-  body: {
-    fontSize: 24,
-    color: '#666666'
-  },
-  loadingContainer: {
-    flex: 1,
-    backgroundColor: "#f5f5f5",
-    paddingTop: StatusBar.currentHeight,
-    justifyContent: "center",
-    alignItems: "center"
-  }
-});
---
+++ screens/AboutScreen.js
@@ -0,0 +1,101 @@
+import { useDeviceOrientation } from '@react-native-community/hooks';
+import { useNavigation } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
+import {
+    ActivityIndicator,
+    Button,
+    FlatList, Platform,
+    SafeAreaView, StatusBar, StyleSheet, Text, View
+} from 'react-native';
+
+export default function App() {
+
+    const navigation = useNavigation();
+    const [postList, setPostList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [refreshing, setRefreshing] = useState(false);
+
+    const orientation = useDeviceOrientation()
+    console.log('orientation is:', orientation)
+
+    const fetchData = async (limit = 1) => {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
+        const data = await response.json();
+        setPostList(data);
+        setIsLoading(false);
+    }
+
+    const handleRefresh = () => {
+        setRefreshing(true);
+        fetchData(20);
+        setRefreshing(false);
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetchData();
+        }, 1500);
+    }, [])
+
+    if (isLoading) {
+        return (
+            <SafeAreaView style={styles.loadingContainer}>
+                <ActivityIndicator size="large" color="0000ff"></ActivityIndicator>
+                <Text>Loading...</Text>
+            </SafeAreaView>
+        )
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.listContainer}>
+                <Button title='Go to home' onPress={() => navigation.navigate("Home")}></Button>
+                <FlatList
+                    data={postList}
+                    refreshing={refreshing}
+                    onRefresh={handleRefresh}
+                    renderItem={({ item }) => {
+                        return (
+                            <View style={styles.card}>
+                                <Text style={styles.title}>{item.title}</Text>
+                                <Text style={styles.body}>{item.body}</Text>
+                            </View>
+                        )
+                    }} />
+            </View>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f5f5f5',
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+    },
+    listContainer: {
+        flex: 1,
+        paddingHorizontal: 16
+    },
+    card: {
+        backgroundColor: 'white',
+        padding: 16,
+        borderRadius: 8,
+        borderWidth: 1,
+        margin: 10
+    },
+    title: {
+        fontSize: 30
+    },
+    body: {
+        fontSize: 24,
+        color: '#666666'
+    },
+    loadingContainer: {
+        flex: 1,
+        backgroundColor: "#f5f5f5",
+        paddingTop: StatusBar.currentHeight,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+});
